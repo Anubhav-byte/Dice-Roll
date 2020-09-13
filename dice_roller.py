2d15569531b2baa1cadaa2c965e5b3b3061a74db
Anubhav Purohit <purohitanubhav@yahoo.com>
2020-09-13 19:45:27 +0530

    Tutorial Complete

--- dice_roller.py
+++ dice_roller.py
@@ -2,12 +2,23 @@ import random
 def main():
     roll=5
     dice_rolls=int(input("Enter the number of dices you want to roll"))
+    dice_side=int(input("How many sides are the dice ?"))
     dice_sum=0
     for i in range(dice_rolls):
-        roll=random.randint(1,6)
+        roll=random.randint(1,dice_side)
         dice_sum+=roll
-        print("You rolled a {}".format(roll))
+        c_statement=custom_statement(roll)
+        print(c_statement)
     print("You have rolled a total of {}".format(dice_sum))
+
+
+def custom_statement(roll):
+    switcher={
+    1:"You rolled a {}! Critical failure!".format(roll) ,
+    6:"You rolled a {}! Critical Success!".format(roll)
+
+    }
+    return switcher.get(roll, "You rolled a {}!".format(roll))
 if __name__== "__main__":
 
     main()
